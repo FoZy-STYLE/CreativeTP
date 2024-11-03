0686726a239467fabdff895afe2794ed0c27f550
FoZy STYLE <you@example.com>
2024-11-03 23:18:09 +0200

    Initial commit

---
+++ src/main/java/me/fozystyle/creativetp/CreativeTP.java
@@ -0,0 +1,23 @@
+package me.fozystyle.creativetp;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CreativeTP implements ModInitializer {
+	public static final String MOD_ID = "creativetp";
+
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+		LOGGER.info("FoZy Industries CompSMP CreativeTP mod has initiated");
+
+
+	}
+}
---
+++ src/main/java/me/fozystyle/creativetp/CreativeTPDataGenerator.java
@@ -0,0 +1,11 @@
+package me.fozystyle.creativetp;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CreativeTPDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
